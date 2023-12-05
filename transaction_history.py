95846912b13466bf00f86e6da1832f2d2f357192
Kevn Cornl <123711097+codekvnnn@users.noreply.github.com>
2023-12-05 15:17:12 -0500

    Create transaction_history.py

---
+++ transaction_history.py
@@ -0,0 +1,8 @@
+class TransactionHistory:
+    def __init__(self, db):
+        self.db = db
+
+    def get_history(self, account_number):
+        # Fetch and return transaction history from the database
+        pass
+```
