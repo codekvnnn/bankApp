eaf2ecd95736362f341ca5c84175e6b11475d6c8
Kevn Cornl <123711097+codekvnnn@users.noreply.github.com>
2023-12-05 15:18:01 -0500

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,8 @@
+def validate_account_number(account_number):
+    # Validate account number format
+    pass
+
+def handle_exceptions(function):
+    # Decorator for handling exceptions in other functions
+    pass
+```
